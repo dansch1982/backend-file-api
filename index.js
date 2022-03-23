6f2af8c4ce26a7225d3abf5229ac63c30c9b241e
dansch1982 <dansch1982@gmail.com>
2022-03-23 22:12:38 +0100

    sum

--- index.js
+++ index.js
@@ -1,83 +1,139 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const path = require('path');
 const mysql = require('mysql2');
 
 http.createServer((req, res) => {
-    console.log(req.url)
-    if (!path.extname(req.url)) {
-        const parts = req.url.split('/').filter(Boolean)
-        const part = parts.length <= 0 ? "index" : parts[parts.length - 1]
-        const con = createConnection('localhost', 'root', "mycms")
-        const query = `SELECT * FROM content WHERE LOWER(REPLACE(content.PageName, " ","") ) = "${part.toLowerCase()}"`
+    const host = 'http' + '://' + req.headers.host + '/';
+    const url = new URL(req.url, host);
+    console.log(`${req.method}: ${url.pathname}`);
+    const parts = url.pathname.split('/').filter(Boolean)
 
-        con.query(query, (err, results, fields) => {
-            let html;
-            if (results.length > 0) {
-                const page = results[0]
-                html = createPage(page)
-            }
-            res.writeHead(200, {
-                "Content-Type": "text/html"
-            })
-            res.end(html)
-        })
-    } else if (['.jpg','.png'].includes(path.extname(req.url))) {
-        const file = path.join(__dirname, req.url)
-        const fileStream = fs.createReadStream(file);
-        res.writeHead(200, {
-            "Content-Type": "image/png"
-        });
-        fileStream.pipe(res);
-    } else if (['.css'].includes(path.extname(req.url))) {
-        const file = path.join(__dirname, req.url)
-        const fileStream = fs.createReadStream(file);
-        res.writeHead(200, {
-            "Content-Type": "text/css"
-        });
-        fileStream.pipe(res);
-    } else {
-        res.writeHead(200, {
-            "Content-Type": "text/html"
-        })
-        res.end()
+    switch (req.method) {
+        case "GET":
+            get(res, parts);
+            break;
+        case "POST":
+            post(req, res, parts);
+            break;
+        default:
+            resolve(res, "something went wrong.")
+            break;
     }
+
 }).listen(8080);
 
-function createConnection(host, user, db) {
-    const connection = mysql.createConnection({
-        host: host,
-        user: user,
-        database: db
-    });
-    return connection;
+function get(res, parts) {
+
+    const file = getFileName(parts);
+    
+    fs.readFile(file, (err, data) => {
+        let response;
+        
+        if (err) {
+            const files = fs.readdirSync(path.join(__dirname, "json"))
+
+            for (let i = 0; i < files.length; i++) {
+                files[i] = path.parse(files[i]).name
+            }
+
+            response = `Available entrypoints: ${files.join(', ')}.`
+
+        } else {
+
+            response = getDeepObject(data, parts)
+
+        }
+        resolve(res, JSON.stringify(response));
+    })
 }
 
-function createPage(page) {
-    const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>${page.PageName}</title>
-    </head>
-    <body>
-    <section>
-    <h1>${page.Header}</h1>
-    <p>${page.Text}</p>
-    </section>
-    <section>
-    <h2>${page.ImageText}</h2>
-    <img src="${page.Image}" alt="${page.Image.split('.')[0]}">
-    </section>
-    <section>
-    <a href="${page.Link}">${page.LinkText}</a>
-    </section>
-    </body>
-    </html>
-    `
-    return html
+function post(req, res, parts) {
+
+    const file = getFileName(parts);
+    let object;
+
+    fs.readFile(file, async (err, data) => {
+
+        let response;
+
+        if (err) {
+            console.log(err.toString())
+        } else {
+            object = getDeepObject(data, parts)
+        }
+
+        if (object) {
+            const buffers = [];
+            for await (const chunk of req) {
+                buffers.push(chunk);
+            }
+            const body = JSON.parse(Buffer.concat(buffers));
+            if (body) {
+                for (const key in body) {
+                    const element = body[key];
+                    if (!object[key]) {
+                        object[key] = element;
+                    }
+                }
+            }
+            data = JSON.parse(data)
+            const refArray = [];
+            for (let i = 0; i < parts.length; i++) {
+                if (refArray.length <= 0) {
+                    refArray.push(data[parts[i]])
+                } else {
+                    refArray.push(refArray[i-1][parts[i]])
+                }
+            }
+            //console.log(object)
+            //console.log(refArray[refArray.length-1])
+            const old = refArray[refArray.length-2][parts[parts.length-1]];
+            refArray[refArray.length-2][parts[parts.length-1]] = object
+            const notOld = refArray[refArray.length-2][parts[parts.length-1]];
+            //console.log(data)
+            //response = JSON.stringify(old) + "" + JSON.stringify(notOld)
+            response = "Data added."
+        }
+
+        resolve(res, response)
+    })
+
+}
+
+function resolve(res, data) {
+    res.writeHead(200, {
+        "Content-Type": "text/json",
+        "Access-Control-Allow-Origin": "*"
+    })
+    res.end(data);
+}
+
+function getFileName(parts) {
+    const file = path.join(__dirname, "json", (parts[0] ? parts.shift() : "") + ".json")
+    return file;
+}
+
+function getDeepObject(data, parts) {
+
+    let response;
+    try {
+        response = JSON.parse(data)
+    } catch (error) {
+        response = data
+    }
+
+    if (parts.length) {
+        for (let i = 0; i < parts.length; i++) {
+            const part = parts[i];
+            if (response[part]) {
+                response = response[part]
+            } else {
+                response = null
+                break;
+            }
+        }
+    }
+    return response
 }
