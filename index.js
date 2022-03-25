530e855b4e7515d0b9823525cb3b9e88cf6791f9
dansch1982 <dansch1982@gmail.com>
2022-03-25 13:27:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 const path = require('path');
-const mysql = require('mysql2');
 
 http.createServer((req, res) => {
     const host = 'http' + '://' + req.headers.host + '/';
@@ -18,7 +16,7 @@ http.createServer((req, res) => {
             post(req, res, parts);
             break;
         default:
-            resolve(res, "something went wrong.")
+            resolve(res, error("Something went wrong."), "application/json")
             break;
     }
 
@@ -27,10 +25,10 @@ http.createServer((req, res) => {
 function get(res, parts) {
 
     const file = getFileName(parts);
-    
+
     fs.readFile(file, (err, data) => {
         let response;
-        
+
         if (err) {
             const files = fs.readdirSync(path.join(__dirname, "json"))
 
@@ -38,14 +36,14 @@ function get(res, parts) {
                 files[i] = path.parse(files[i]).name
             }
 
-            response = `Available entrypoints: ${files.join(', ')}.`
+            response = error(`Available entrypoints: ${files.join(', ')}.`)
 
         } else {
 
             response = getDeepObject(data, parts)
 
         }
-        resolve(res, JSON.stringify(response));
+        resolve(res, response, "application/json");
     })
 }
 
@@ -55,63 +53,91 @@ function post(req, res, parts) {
     let object;
 
     fs.readFile(file, async (err, data) => {
-
         let response;
-
         if (err) {
-            console.log(err.toString())
+            // create file ?
+            return resolve(res, err, "application/json")
         } else {
             object = getDeepObject(data, parts)
         }
-
-        if (object) {
-            const buffers = [];
-            for await (const chunk of req) {
-                buffers.push(chunk);
+        if (!object) {
+            // create deep object?
+            return resolve(res, error("Incorrect path"), "application/json")
+        }
+        const buffers = [];
+        for await (const chunk of req) {
+            buffers.push(chunk);
+        }
+        let body;
+        try {
+            body = JSON.parse(Buffer.concat(buffers));
+        } catch (err) {
+            return resolve(res, error({
+                "nobody": "",
+                "error": "nobody"
+            }), "application/json")
+        }
+        for (const key in body) {
+            const element = body[key];
+            if (!object[key]) {
+                object[key] = element;
             }
-            const body = JSON.parse(Buffer.concat(buffers));
-            if (body) {
-                for (const key in body) {
-                    const element = body[key];
-                    if (!object[key]) {
-                        object[key] = element;
+        }
+        data = JSON.parse(data)
+        const refArray = [data];
+        for (let i = 1; i < parts.length; i++) {
+            refArray.push(refArray[i - 1][parts[i]])
+        }
+        let before, after;
+        if (parts.length <= 1) {
+            before = refArray[0];
+            data = object
+            after = object;
+
+        } else {
+            before = refArray[refArray.length - 2][parts[parts.length - 1]];
+            refArray[refArray.length - 2][parts[parts.length - 1]] = object
+            after = refArray[refArray.length - 2][parts[parts.length - 1]];
+        }
+        if (JSON.stringify(before) === JSON.stringify(object)) {
+            resolve(res, error("No data added."), "application/json")
+        } else {
+            fs.writeFile(file, JSON.stringify(data), (err) => {
+
+                if (err) {
+                    response = error(err.toString())
+                } else {
+                    response = {
+                        "succes": "Data added.",
+                        "before": before,
+                        "after": object
                     }
                 }
-            }
-            data = JSON.parse(data)
-            const refArray = [];
-            for (let i = 0; i < parts.length; i++) {
-                if (refArray.length <= 0) {
-                    refArray.push(data[parts[i]])
-                } else {
-                    refArray.push(refArray[i-1][parts[i]])
-                }
-            }
-            //console.log(object)
-            //console.log(refArray[refArray.length-1])
-            const old = refArray[refArray.length-2][parts[parts.length-1]];
-            refArray[refArray.length-2][parts[parts.length-1]] = object
-            const notOld = refArray[refArray.length-2][parts[parts.length-1]];
-            //console.log(data)
-            //response = JSON.stringify(old) + "" + JSON.stringify(notOld)
-            response = "Data added."
+                resolve(res, response, "application/json")
+            })
         }
-
-        resolve(res, response)
     })
 
 }
 
-function resolve(res, data) {
+function resolve(res, data, type) {
     res.writeHead(200, {
-        "Content-Type": "text/json",
+        "Content-Type": type,
         "Access-Control-Allow-Origin": "*"
     })
+    try {
+        JSON.parse(data)
+    } catch (error) {
+        data = JSON.stringify(data)
+    }
+    if (typeof data !== "string") {
+        data = new String(data).toString()
+    }
     res.end(data);
 }
 
 function getFileName(parts) {
-    const file = path.join(__dirname, "json", (parts[0] ? parts.shift() : "") + ".json")
+    const file = path.join(__dirname, "json", (parts[0] ? parts[0] : "") + ".json")
     return file;
 }
 
@@ -124,8 +150,8 @@ function getDeepObject(data, parts) {
         response = data
     }
 
-    if (parts.length) {
-        for (let i = 0; i < parts.length; i++) {
+    if (parts.length - 1) {
+        for (let i = 1; i < parts.length; i++) {
             const part = parts[i];
             if (response[part]) {
                 response = response[part]
@@ -136,4 +162,11 @@ function getDeepObject(data, parts) {
         }
     }
     return response
+}
+
+function error(message) {
+    const object = {
+        "error": message
+    }
+    return object
 }
