acf0f06241d2e8c7651314dfc3ecd23fd9b7d579
dansch1982 <dansch1982@gmail.com>
2022-04-04 08:04:19 +0200

    Update incorrectEntry.js

--- services/incorrectEntry.js
+++ services/incorrectEntry.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-const Response = require('../services/Response')
-
 function incorrectEntry(res) {
     const files = fs.readdirSync(path.join(__dirname, "/../json"))
 
@@ -10,7 +8,6 @@ function incorrectEntry(res) {
         files[i] = path.parse(files[i]).name
     }
 
-    res = new Response(res)
     res.status(404).json({"entrypoints" : files})
 }
 module.exports = incorrectEntry
