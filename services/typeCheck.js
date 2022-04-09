d73c50b47536c6f6b6f248140aedd3ee8893981e
dansch1982 <dansch1982@gmail.com>
2022-04-09 19:32:46 +0200

    Update typeCheck.js

--- services/typeCheck.js
+++ services/typeCheck.js
@@ -1,12 +1,14 @@
 function typeCheck(args, ...expected) {
     const received = []
-    if(!expected.every((arg, index) => {
-        const [,value] = Object.entries(args)[index]
+    if(!args || !expected.every((arg, index) => {
+        const value = Object.values(args)[index]
         expected[index] = Object.prototype.toString.call(arg)
         received[index] = Object.prototype.toString.call(value)
         return received[index] === expected[index]
     })) {
         throw new TypeError(`Expected ${expected}, received ${received}.`);
+    } else {
+        return true
     }
 }
 module.exports = typeCheck
