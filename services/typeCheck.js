65286af51316783c26cd1ff47c6e75c66f62acf7
dansch1982 <dansch1982@gmail.com>
2022-04-09 21:39:19 +0200

    Update typeCheck.js

--- services/typeCheck.js
+++ services/typeCheck.js
@@ -1,14 +1,34 @@
 function typeCheck(args, ...expected) {
     const received = []
     if(!args || !expected.every((arg, index) => {
+        
         const value = Object.values(args)[index]
-        expected[index] = Object.prototype.toString.call(arg)
-        received[index] = Object.prototype.toString.call(value)
+        received[index] = getType(value)
+        
+        if (Array.isArray(arg) && arg.length > 0) {
+            arg.forEach((item,index) => {
+                arg[index] = getType(item)
+            })
+            if (arg.indexOf(received[index]) >= 0) {
+                expected[index] = received[index]
+            }
+        } else {
+            expected[index] = getType(arg)
+        }
+
         return received[index] === expected[index]
     })) {
-        throw new TypeError(`Expected ${expected}, received ${received}.`);
+        let expectedString = "";
+        expected.forEach(item => {
+            expectedString += Array.isArray(item) ? `[${item.join('|')}], ` : `${item}, `
+        })
+        expectedString = expectedString.slice(0, -2)
+        throw new TypeError(`Expected ${expectedString}. Received ${received.join(', ')}.`);
     } else {
         return true
+    }
+    function getType(item) {
+        return Object.prototype.toString.call(item).replace(/^\[object |\]$/g, '').toLowerCase()
     }
 }
 module.exports = typeCheck
