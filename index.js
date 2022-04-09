51310997e7edd6a869f588f9d6fbc7b154e6d898
dansch1982 <dansch1982@gmail.com>
2022-04-09 19:32:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,22 @@
-const path = require('path')
-
 const server = require('./services/server')
-const switcher = require('./services/switcher');
-const getURL = require('./services/getURL')
 
 server.listen(8080, (req, res) => {
-
-    res.setHeader('Access-Control-Allow-Origin', '*')
-
-    const url = getURL(req)
-
-    if (url.code) {
-        return res.status(500).text('Something went wrong.')
-    }
-
-    const file = path.parse(path.join(__dirname, url.pathname))
-    file.path = path.join(file.dir, file.base)
-    console.log(`${req.method}: ${url.pathname}`);
-
-    if (req.method === "GET" && file.ext) {
-        return res.file(file)
-    }
-
-    const parts = url.pathname.split('/').filter(Boolean)
-
-    switcher.addObjective("OPTIONS", () => {
-        const options = require('./controllers/options')
-        options(res)
-    })
-
-    switcher.addObjective("GET", () => {
-        const get = require('./controllers/get')
-        get(res, parts);
-    })
-
-    switcher.addObjective("PUT", () => {
-        switcher.switch('POST', true)
-    })
-
-    switcher.addObjective("POST", (...args) => {
-        const [put] = args
-        const post = require('./controllers/post')
-        post(req, res, parts, put);
-    })
-
-    switcher.addObjective("DELETE", () => {
-        const deletePost = require('./controllers/deletePost')
-        deletePost(res, parts)
-    })
-    if (!switcher.switch(req.method)) {
-        res.status(405).text("Something went wrong.")
-    }
 })
-
-//TESTING
-const apple = "apple"
-const banana = "banana"
 
 server.setHeader('Access-Control-Allow-Origin', '*')
 
-function testGet(req, res, args) {
-    res.status(200).text("GETTING: " + args.join(", "))
-}
-server.get(apple, testGet, "fruit", "apple")
+server.static("public")
 
-function testPost(req, res, args) {
-    res.status(200).text("POSTING: " + args.join(", "))
-}
-server.post("test", testPost, "fruit", "apple")
+const get = require('./controllers/get')
+server.default('get', get)
 
-server.get('/', (req, res) => {
-    res.status(200).text("ok")
-})
-server.get('test', (req, res) => {
-    server.setHeader("fruit", "banana")
-    res.status(201).json({"test" : "value"})
-})
+const post = require('./controllers/post')
+server.default('post', post)
 
-server.post('/', (req, res) => {
-    res.status(200).text("ok")
-})
+server.default('put', post, true)
 
-server.delete('/', (req, res) => {
-    res.status(200).text("delete")
-})
+const deletePost = require('./controllers/deletePost')
+server.default('delete', deletePost)
 
-server.default("get", (req, res) => {
-    res.status(200).text("Default GET")
-})
-
-server.default("POST", (req, res, args) => {
-    res.status(200).text("Default POST with: " + args.join(", "))
-}, apple, banana)
-
-server.public("public")
+const options = require('./controllers/options')
+server.default('options', options)
