a9a410e3ffefacee4fbedf0094b2bb6cafea8648
dansch1982 <dansch1982@gmail.com>
2022-04-11 12:45:21 +0200

    Delete options.js

--- controllers/options.js
+++
@@ -1,7 +0,0 @@
-function options(req, res) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).end()
-}
-
-module.exports = options
